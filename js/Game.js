cd2f9f52d2f4ca41caf527091eb5855b93a7abc5
rt-brown <47324919+rt-brown@users.noreply.github.com>
2019-09-15 05:46:33 -0700

    testing createPhrases()

--- js/Game.js
+++ js/Game.js
@@ -5,12 +5,28 @@
  class Game{
      constructor(){
          this.missed = 0;
-         this.phrases = [];
+         this.phrases = createPhrases();
          this.activePhrase = null;
      }
 
-     createPhrase(){
-         const newPhrases = [];
-         
-     }
- }
+     createPhrases(){
+         const listOfphrases = 
+         [
+             'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
+             'It is during our darkest moments that we must focus to see the light.',
+             'Never let the fear of striking out keep you from playing the game.',
+             "You miss 100% of the shots you don't take.",
+             "Believe you can and you're halfway there."
+        ];
+
+        const newListofPhrases = [];
+
+        listOfphrases
+        .forEach((phrase) => {
+            let newPhraseObject = new Phrase(phrase);
+            newListofPhrases.push(newPhraseObject);
+        })
+        
+        return newListofPhrases;
+     };
+ };
